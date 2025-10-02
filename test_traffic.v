b5950f9a85a24002ae6b65850c57b48146f417d3
DwarakanathaReddyGajjala <163575842+DwarakanathaReddyGajjala@users.noreply.github.com>
2025-10-02 17:48:10 +0530

    Update test_traffic.v

--- test_traffic.v
+++ test_traffic.v
@@ -1,19 +1,28 @@
-`include"trafficcontroller.v"
 module test_traffic;
-reg clk,x,start;
-wire [2:0] highway,country;
-traffic a0(x,clk,highway,country,start);
-always
-#5 clk=~clk;
-initial
-begin
-clk=0;start=0;
-#2 start=1;#10 x=1;#70 x=0;
+reg clk, x, start;
+wire [2:0] highway, country;
+
+// Instantiate the traffic controller
+traffic_controller traffic(x, clk, highway, country, start);
+
+// Clock generation
+always #5 clk = ~clk;
+
+// Stimulus
+initial begin
+  clk = 0; 
+  start = 0;
+  #2 start = 1;     // Start the system
+  #10 x = 1;        // Country road request (highway must yield later)
+  #70 x = 0;        // Request removed
 end
-initial
-begin
-$monitor("highway=%b,country=%b",highway,country);
-#500 $finish;
+
+// Monitoring and waveform dump
+initial begin
+  $dumpfile("traffic.vcd");        // Save waveform
+  $dumpvars(0, test_traffic);      // Dump all signals
+  $monitor("highway=%b, country=%b", highway, country); // Print values
+  #500 $finish;                    // Stop simulation after 500 time units
 end
 
 endmodule
